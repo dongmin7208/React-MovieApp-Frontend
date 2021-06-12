17ef7f5e2e7a7d3730e6faaa32608eb3bd4a89b6
dongmin7208 <sayg3o3d@gmail.com>
2021-06-13 01:44:53 +0900

    how to use array + map Implement JavaScript__map to picture load

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import React from 'react';
 
-function Food({ favourite }) {
-  return <h1>i like {favourite}</h1>;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>i like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: 'natto',
+    image:
+      'https://cdn.pixabay.com/photo/2013/08/01/07/17/natto-169229_960_720.jpg',
+  },
+  {
+    name: 'kimchi',
+    image:
+      'https://media.istockphoto.com/photos/plate-full-of-kimchi-and-chopsticks-on-side-picture-id158554261?s=612x612',
+  },
+  {
+    name: 'greensarada',
+    image:
+      'https://cdn.pixabay.com/photo/2016/10/02/20/02/green-1710328_960_720.jpg',
+  },
+  {
+    name: 'yariika',
+    image:
+      'https://media.istockphoto.com/photos/-picture-id820227544?s=612x612',
+  },
+  {
+    name: 'iwashi',
+    image:
+      'https://cdn.pixabay.com/photo/2019/04/30/16/45/restaurant-4168987_960_720.jpg',
+  },
+  {
+    name: 'daigakuemo',
+    image:
+      'https://cdn.pixabay.com/photo/2016/09/13/08/51/sweet-potato-1666707_960_720.jpg',
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>hello dongdong</h1>
-      <Food favourite="kimchi" />
-      <Food favourite="natto" />
-      <Food favourite="greensarada" />
-      <Food favourite="yariika" />
-      <Food favourite="iwashi" />
-      <Food favourite="daigakuemo" />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
