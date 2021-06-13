a5287619dc212f9a78280f761cc25d36f981b1fd
dongmin7208 <sayg3o3d@gmail.com>
2021-06-13 13:59:35 +0900

    How to use 'state' Add Buttons

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component {
-  state = {};
+  state = {
+    count: 0,
+  };
   render() {
-    return <h1> Im a class component</h1>;
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button>Add</button>
+        <button>Minus</button>
+      </div>
+    );
   }
 }
 
