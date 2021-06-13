7d6ced67180964c6346e99397f5495b20fd58d41
dongmin7208 <sayg3o3d@gmail.com>
2021-06-13 13:01:17 +0900

    Installed prop-types Because I want to deliver more props. I installed 'npm i prop-types' what this does is to check the 'props' that I received and prevent my mistakes.

--- src/App.js
+++ src/App.js
@@ -6,36 +6,42 @@ const foodILike = [
     name: 'natto',
     image:
       'https://cdn.pixabay.com/photo/2013/08/01/07/17/natto-169229_960_720.jpg',
+    rating: 5,
   },
   {
     id: 2,
     name: 'kimchi',
     image:
       'https://media.istockphoto.com/photos/plate-full-of-kimchi-and-chopsticks-on-side-picture-id158554261?s=612x612',
+    rating: 4.8,
   },
   {
     id: 3,
     name: 'greensarada',
     image:
       'https://cdn.pixabay.com/photo/2016/10/02/20/02/green-1710328_960_720.jpg',
+    rating: 3.5,
   },
   {
     id: 4,
     name: 'yariika',
     image:
       'https://media.istockphoto.com/photos/-picture-id820227544?s=612x612',
+    rating: 3.5,
   },
   {
     id: 5,
     name: 'iwashi',
     image:
       'https://cdn.pixabay.com/photo/2019/04/30/16/45/restaurant-4168987_960_720.jpg',
+    rating: 4.0,
   },
   {
     id: 6,
     name: 'daigakuemo',
     image:
       'https://cdn.pixabay.com/photo/2016/09/13/08/51/sweet-potato-1666707_960_720.jpg',
+    rating: 1.5,
   },
 ];
 
