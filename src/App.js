101f66cce34ba6b4ff2e33977c22fa7fbeb76aed
dongmin7208 <sayg3o3d@gmail.com>
2021-06-13 12:51:37 +0900

    how to use alt,Uniqueness

--- src/App.js
+++ src/App.js
@@ -1,53 +1,61 @@
 import React from 'react';
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>i like {name}</h2>
-      <img src={picture} />
-    </div>
-  );
-}
-
 const foodILike = [
   {
+    id: 1,
     name: 'natto',
     image:
       'https://cdn.pixabay.com/photo/2013/08/01/07/17/natto-169229_960_720.jpg',
   },
   {
+    id: 2,
     name: 'kimchi',
     image:
       'https://media.istockphoto.com/photos/plate-full-of-kimchi-and-chopsticks-on-side-picture-id158554261?s=612x612',
   },
   {
+    id: 3,
     name: 'greensarada',
     image:
       'https://cdn.pixabay.com/photo/2016/10/02/20/02/green-1710328_960_720.jpg',
   },
   {
+    id: 4,
     name: 'yariika',
     image:
       'https://media.istockphoto.com/photos/-picture-id820227544?s=612x612',
   },
   {
+    id: 5,
     name: 'iwashi',
     image:
       'https://cdn.pixabay.com/photo/2019/04/30/16/45/restaurant-4168987_960_720.jpg',
   },
   {
+    id: 6,
     name: 'daigakuemo',
     image:
       'https://cdn.pixabay.com/photo/2016/09/13/08/51/sweet-potato-1666707_960_720.jpg',
   },
 ];
 
-function renderFood(dish) {
-  console.log(dish);
-  return <Food name={dish.name} picture={dish.image} />;
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>i like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
 
 function App() {
-  return <div>{foodILike.map(renderFood)}</div>;
+  return (
+    <div>
+      {foodILike.map((dish) => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
+      ))}
+    </div>
+  );
 }
+
 export default App;
