dd537e78e6159af1cb8f523f6e00a30c08eeb281
dongmin7208 <sayg3o3d@gmail.com>
2021-06-14 12:05:29 +0900

    'how to javascript ternary operator_isLoading' use 条件(三項)演算子

--- src/App.js
+++ src/App.js
@@ -2,34 +2,11 @@ import React from 'react';
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
   };
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
-  componentDidMount() {
-    console.log('component rendered');
-  }
-  componentDidUpdate() {
-    console.log('I just updated');
-  }
-  componentWillUnmount() {
-    console.log('Goodbye, cruel world');
-  }
-
   render() {
-    console.log('I m rendering');
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading' : 'we are ready'}</div>;
   }
 }
-
 export default App;
