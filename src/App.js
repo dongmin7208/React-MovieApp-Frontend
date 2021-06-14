3c5e8b45d061af12c43b979af9d71d756d03a7c6
dongmin7208 <sayg3o3d@gmail.com>
2021-06-14 12:46:45 +0900

    Connecting 'DATA' of 'API'

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ class App extends React.Component {
     movies: [],
   };
   getMovies = async () => {
-    const movies = await axios.get(
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
       'https://yts-proxy.nomadcoders1.now.sh/list_movies.json',
     );
+    console.log(movies);
   };
 
   componentDidMount() {
