28d91215dc634c5faf180557d561cf24e6381608
dongmin7208 <sayg3o3d@gmail.com>
2021-06-14 15:58:20 +0900

    Add img(poster),alt

--- src/Movie.js
+++ src/Movie.js
@@ -4,9 +4,12 @@ import PropTypes from 'prop-types';
 function Movie({ id, year, title, summary, poster }) {
   return (
     <div class="movie">
-      <h3 class="movie__title">{title}</h3>
-      <h5 class="movie__year">{year}</h5>
-      <p class="movie__summary">{summary}</p>
+      <img src={poster} alt={title} title={title} />
+      <div class="movie__data">
+        <h3 class="movie__title">{title}</h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+      </div>
     </div>
   );
 }
