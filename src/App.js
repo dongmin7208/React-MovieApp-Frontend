09ce89b1c564b56469cbbfc490ecc00cdbf226d6
dongmin7208 <sayg3o3d@gmail.com>
2021-06-14 11:22:46 +0900

    'State' dont  the recommended way.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ class App extends React.Component {
   state = {
     count: 0,
   };
+  add = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  minus = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
   render() {
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
-        <button>Add</button>
-        <button>Minus</button>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
       </div>
     );
   }
