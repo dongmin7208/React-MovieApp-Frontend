c29f03ede269e5548e192afb65dd6119904a9a96
dongmin7208 <sayg3o3d@gmail.com>
2021-06-14 12:14:39 +0900

    how to DidMount + isLoading It is not necessary to declare a 'state' to be used in the future.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import React from 'react';
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
   };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
   render() {
     const { isLoading } = this.state;
-    return <div>{isLoading ? 'Loading' : 'we are ready'}</div>;
+    return <div>{isLoading ? 'Loading....中' : 'we are ready'}</div>;
   }
 }
 export default App;
