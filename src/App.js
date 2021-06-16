2c04a841832e3e549258a9e5405ee59e1e6fe3ca
dongmin7208 <sayg3o3d@gmail.com>
2021-06-16 13:41:53 +0900

    Add path Route path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
-
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
 function App() {
-  return <span>something</span>;
+  return (
+    <HashRouter>
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 export default App;
